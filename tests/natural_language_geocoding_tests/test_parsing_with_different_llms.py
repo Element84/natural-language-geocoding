7c7a465448cfc5fe6f3d3a9746c8c15994f29ad7
Jason Gilman <jason@element84.com>
2025-02-20 12:22:34 -0500

    Feature/maritime improvements (#8)
    * Adding support of buffers with nautical miles
    * Fixes an issue with named locations an adding in subportions that aren't valid against the schema.
    * Adding instructions for how to handle ports

--- tests/natural_language_geocoding_tests/test_parsing_with_different_llms.py
+++ tests/natural_language_geocoding_tests/test_parsing_with_different_llms.py
@@ -7,35 +7,49 @@ from e84_geoai_common.llm.models import (
 from e84_geoai_common.llm.tests.mock_bedrock import USE_REAL_BEDROCK_CLIENT
 
 import natural_language_geocoding
-from natural_language_geocoding.models import Between, Difference, NamedPlace
+from natural_language_geocoding.models import (
+    AnySpatialNodeType,
+    Between,
+    CoastOf,
+    Difference,
+    NamedPlace,
+)
 
 parse_spatial_node = natural_language_geocoding._parse_spatial_node_from_text  # type: ignore[reportPrivateUsage]
 
 
-_TEXT = "between Alpha and Bravo except for Charlie"
-
-_EXPECTED_SPATIAL_NODE = Difference(
-    child_node_1=Between(
-        child_node_1=NamedPlace(name="Alpha"),
-        child_node_2=NamedPlace(name="Bravo"),
+EXAMPLES: list[tuple[str, AnySpatialNodeType]] = [
+    (
+        "between Alpha and Bravo except for Charlie",
+        Difference(
+            child_node_1=Between(
+                child_node_1=NamedPlace(name="Alpha"),
+                child_node_2=NamedPlace(name="Bravo"),
+            ),
+            child_node_2=NamedPlace(name="Charlie"),
+        ),
     ),
-    child_node_2=NamedPlace(name="Charlie"),
-)
+    ("the port of miami", CoastOf(child_node=NamedPlace(name="Miami Florida"))),
+]
 
 
 @pytest.mark.skipif(
     not USE_REAL_BEDROCK_CLIENT, reason="Test is only run when using real bedrock clients"
 )
-def test_nova_geocoding() -> None:
+@pytest.mark.parametrize(("example_pair"), EXAMPLES)
+def test_nova_geocoding(example_pair: tuple[str, AnySpatialNodeType]) -> None:
     llm = BedrockNovaLLM()
-    node = parse_spatial_node(llm, _TEXT)
-    assert node.root == _EXPECTED_SPATIAL_NODE
+    query, expected_node = example_pair
+    node = parse_spatial_node(llm, query)
+    assert node.root.model_dump() == expected_node.model_dump()
 
 
 @pytest.mark.skipif(
     not USE_REAL_BEDROCK_CLIENT, reason="Test is only run when using real bedrock clients"
 )
-def test_claude_geocoding() -> None:
+@pytest.mark.parametrize(("example_pair"), EXAMPLES)
+def test_claude_geocoding(example_pair: tuple[str, AnySpatialNodeType]) -> None:
     llm = BedrockClaudeLLM(CLAUDE_BEDROCK_MODEL_IDS["Claude 3.5 Sonnet"])
-    node = parse_spatial_node(llm, _TEXT)
-    assert node.root == _EXPECTED_SPATIAL_NODE
+    query, expected_node = example_pair
+    node = parse_spatial_node(llm, query)
+    assert node.root.model_dump() == expected_node.model_dump()
