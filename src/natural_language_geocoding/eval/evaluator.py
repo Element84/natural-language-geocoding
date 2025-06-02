72cb3581a5302c0f64a449e6519423732d6be60f
Ryan Bickford <rbickford@element84.com>
2025-06-02 10:27:11 -0400

    fix test values

--- src/natural_language_geocoding/eval/evaluator.py
+++ src/natural_language_geocoding/eval/evaluator.py
@@ -251,7 +251,7 @@ FEATURE_EXAMPLES: list[ExampleEval[AnySpatialNodeType]] = [
         expected_node=Buffer(
             child_node=GeoJSON(
                 geometry = [
-                    [(45, -100), (45, -101), (46, -101)]
+                    [(102.0, 0.0), (103.0, 1.0), (104.0, 0.0), (105.0, 0.0)]
                 ]
             ),
             distance=10,
